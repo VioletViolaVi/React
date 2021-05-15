f1d62c25ae67a0c6a68d9b3e8438ddbe21ad8fce
VioletViola <vivian.v18723@gmail.com>
2021-05-15 19:46:44 +0100

    Complete section 2 of jamming.

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import './App.css';
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
